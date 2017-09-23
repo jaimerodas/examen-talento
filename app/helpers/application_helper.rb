1d17ade511c0e1280d6e71111db3ce97f273e6ac
Jaime Rodas <jaime@rodas.mx>
2017-09-23 18:40:18 -0500

    La aplicación te saluda!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@
 
 # Here we have all of the basic helper functions
 module ApplicationHelper
+  def timed_greeting
+    case Time.now.hour
+    when 0..6 then 'Buenas madrugadas'
+    when 7..11 then 'Buenos días'
+    when 12..19 then 'Buenas tardes'
+    else 'Buenas noches'
+    end
+  end
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe ApplicationHelper do
+  describe '#timed_greeting' do
+    it 'says good early morning' do
+      allow(Time).to receive(:now).and_return(OpenStruct.new(hour: 3))
+      expect(timed_greeting).to eq('Buenas madrugadas')
+    end
+
+    it 'says good morning' do
+      allow(Time).to receive(:now).and_return(OpenStruct.new(hour: 9))
+      expect(timed_greeting).to eq('Buenos días')
+    end
+
+    it 'says good afternoon' do
+      allow(Time).to receive(:now).and_return(OpenStruct.new(hour: 15))
+      expect(timed_greeting).to eq('Buenas tardes')
+    end
+
+    it 'says good night' do
+      allow(Time).to receive(:now).and_return(OpenStruct.new(hour: 21))
+      expect(timed_greeting).to eq('Buenas noches')
+    end
+  end
+end
