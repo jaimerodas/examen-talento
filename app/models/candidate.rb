8aa1e0e8d27abf8fa2822208102072e9e26fabab
Jaime Rodas <jaime@rodas.mx>
2017-10-01 07:59:33 -0500

    Quita el que last_name sea obligatorio

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -4,7 +4,7 @@
 class Candidate < ApplicationRecord
   belongs_to :opening, optional: true
 
-  validates_presence_of :email, :service_id, :first_name, :last_name
+  validates_presence_of :email, :service_id, :first_name
   validates_uniqueness_of :email, :service_id
 
   def full_name
--- spec/models/candidate_spec.rb
+++ spec/models/candidate_spec.rb
@@ -12,7 +12,6 @@ describe Candidate, type: :model do
     # Presence
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_presence_of(:first_name) }
-    it { is_expected.to validate_presence_of(:last_name) }
     it { is_expected.to validate_presence_of(:service_id) }
 
     # Uniqueness
